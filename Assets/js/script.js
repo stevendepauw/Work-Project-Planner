8d91d7c5391d209b0d4f804fbc7fe761d4d611c6
unknown <stevendepauw@rocketmail.com>
2022-12-12 22:23:22 -0600

    all but final function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,47 +1,59 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-   
+  let today = dayjs().format("dddd, MMM DD");
+  let theHour = dayjs().hour();
+  let timeDisplay = $("#currentDay");
+  let taskToSave = $(".saveBtn");
+  let ppf = $(".description"); 
+  
+  function displayTime() {
+    timeDisplay.text(today);  
+  }
+
+  function setColor() {
+    ppf.each(function() {
+      let hour = $(this).attr("id")
+
+      if(theHour > hour) {
+        $(this).addClass("past");
+        $(this).removeClass("present");
+      } 
+      else if(theHour == hour) {
+        $(this).addClass("present");
+        $(this).removeClass("future");
+      } 
+      else {
+        $(this).addClass("future");
+        $(this).removeClass("past");
+      } 
+    })
+  }
+
+
+  // function saveProjectsToStorage(projects) {
+  //   localStorage.setItem('projects', JSON.stringify(projects));
+  // }
+
+  // function toDos() {
+  //   let toDoList = localStorage.getItem("toDoList");
+  //   if(toDoList) {
+  //     toDoList = JSON.parse(toDoList);
+  //   } else {
+  //     toDoList = [];
+  //   }
+  //   return toDoList;
+  // }
+
+  $(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).siblings(".description").attr("id");
+ 
+    localStorage.setItem("text", JSON.stringify(text));
+  })
+
+  displayTime();
+  setInterval(displayTime, 60000);
+  setColor();
+  setInterval(setColor, 60000);   
 });
 
-let today = dayjs().format("dddd, MMM DD");
-let theHour = dayjs().hour();
-let timeDisplay = $("#currentDay");
-let taskToSave = $(".saveBtn");
-let ppf = $(".description"); 
-  
-function displayTime() {
-  timeDisplay.text(today);
-}
 
-function setColor() {
-  ppf.each(function() {
-    let hour = $(this).attr("id")
-
-    if(theHour > hour) {
-      $(this).addClass("past");
-      $(this).removeClass("present");
-    } 
-    else if(theHour == hour) {
-      $(this).addClass("present");
-      $(this).removeClass("future");
-    } 
-    else {
-      $(this).addClass("future");
-      $(this).removeClass("past");
-    } 
-  })
-}
-
-$(".saveBtn").on("click", function () {
-  var text = $(this).siblings(".description").val();
-  var time = $(this).siblings(".description").attr("id");
- 
-  localStorage.setItem(time, text);
-})
-
-displayTime();
-setInterval(displayTime, 60000);
-setColor();
-setInterval(setColor, 60000);
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Work-Project-Planner
-finishing code to have a functioning work day and event schedule planner
+# Workday Event Planner
+
+## Description
+
+The purpose of this project was to modify code to make a daily work planner that can dynamically change certain elements and store and pull different values into and from local storage. 
+## Mock-Up
+
+The following images show the web application's appearance and functionality:
+
+![portfolio demo](./assets/images/working-schedule)
+
+## Installation
+
+N/A
+
+## Usage
+The user can enter what daily task he or she might have at a given hour of the workday and that time block will then hold the task in local storage as well as change colors to let the user know if it is passed time, in the current moment, or coming up in the future.
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
